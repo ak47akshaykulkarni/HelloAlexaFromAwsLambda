0cff62d2e1efa71cd351bd396923e48e417551f4
Akshay Kulkarni <ak47.akshaykulkarni@gmail.com>
2018-04-21 12:28:00 +0530

    hello Alex

--- helloAlexaFromAwsLambda/Function.cs
+++ helloAlexaFromAwsLambda/Function.cs
@@ -1,27 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
 using Amazon.Lambda.Core;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
-
 namespace helloAlexaFromAwsLambda
 {
     public class Function
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public SkillResponse FunctionHandler(SkillRequest skillRequest, ILambdaContext context)
         {
-            return input?.ToUpper();
+            if (skillRequest.GetRequestType() != typeof(IntentRequest))
+                return new SkillResponse()
+                {
+                    Response = new ResponseBody
+                    {
+                        ShouldEndSession = true,
+                        OutputSpeech = new PlainTextOutputSpeech
+                        {
+                            Text = "I did not understand what that was, but still, I'll say, Typical Hello World"
+                        }
+                    },
+                    Version = "1.0"
+                };
+            return new SkillResponse()
+            {
+                Response = new ResponseBody()
+                {
+                    OutputSpeech = new PlainTextOutputSpeech() { Text = "Good! There you go... Hello World from AWS Lambda Function" },
+                    ShouldEndSession = true
+                },
+                Version = "1.0"
+            };
         }
     }
 }
